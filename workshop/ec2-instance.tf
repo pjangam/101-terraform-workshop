cfac057f126df6fc840ef2a2522e89312f890327
Pramod Jangam <pjangam@equalexperts.com>
2019-12-07 15:31:45 +0530

    training

--- workshop/ec2-instance.tf
+++ workshop/ec2-instance.tf
@@ -1,12 +1,12 @@
 resource "aws_key_pair" "ssh_key" {
-  key_name = ""
+  key_name = "training"
   public_key = ""
 }
 
 resource "aws_instance" "first_instance" {
-  ami = ""
+  ami = "ami-04b9e92b5572fa0d1"
 
-  subnet_id = ""
+  subnet_id = "subnet-2d5bb123"
 
   key_name = aws_key_pair.ssh_key.key_name
 
